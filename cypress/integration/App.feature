86cc2b8b7d736d295f42ad3fcb9fccc9b3cf25ed
InBrewJ <jason@urawizard.com>
2020-10-04 18:19:38 +0100

    finally, CRA/TS/Cypress/Cucumber/Gherkin/allTS works :okay:

---
+++ cypress/integration/App.feature
@@ -0,0 +1,6 @@
+Feature: Create React App + TS works with Cypress + Gherkin + TS
+
+  @focus
+  Scenario: CRA loads and is tested
+    Given I open the local dev server
+    Then I see the React logo
---
+++ cypress/integration/App/index.ts
@@ -0,0 +1,11 @@
+import { Given, Then } from "cypress-cucumber-preprocessor/steps";
+
+const url: string = "http://localhost:3000";
+
+Given("I open the local dev server", () => {
+  cy.visit(url);
+});
+
+Then("I see the React logo", () => {
+  cy.get(".App-logo");
+});
